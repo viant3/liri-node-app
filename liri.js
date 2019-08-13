2726cc88b74230857e3fc3342e7fd4e14538614b
victor politano <viant3ucf@gmail.com>
2019-08-13 05:41:53 -0400

    added case for spotify - added function to get 10 songs and display info

--- liri.js
+++ liri.js
@@ -1,24 +1,26 @@
-// require("dotenv").config();
+require("dotenv").config();
 
 var keys = require("./keys.js");
 var axios = require("axios");
 var inquirer = require("inquirer");
+var Spotify = require('node-spotify-api');
 
 
-
-
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 var action = process.argv[2];
 var userInput = process.argv[3];
 
 switch (action) {
     case "movie-this":
-          movieInfo();
-    break;
+        movieInfo();
+        break;
     case "concert-this":
-          bandInfo();
-     break;
+        bandInfo();
+        break;
+    case "spotify-this-song":
+        spotifyMe();
+        break;
     default:
         break;
 }
@@ -41,6 +43,33 @@ function movieInfo() {
 function bandInfo() {
     axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
         function (response) {
-            console.log(response);
+            for (let i = 0; i < response.data.length; i++) {
+
+                console.log("Artist: " + (response.data[i].lineup));
+                console.log("Venue Name: " + (response.data[i].venue.name));
+                console.log("Venue Location: " + (response.data[i].venue.city) + ", " + (response.data[i].venue.region) + ", " + (response.data[i].venue.country), "\n");
+            }
         });
 }
+
+function spotifyMe() {
+    spotify
+        .search({ type: 'track', query: userInput, limit: 10 })
+        .then(function (response) {
+            // console.log(response);
+            var songData = response.tracks.items;
+
+            for (let i = 0; i < songData.length; i++) {
+                console.log("Artist Name: " + songData[i].artists[0].name);
+                console.log("Song Name: " + songData[i].name);
+                console.log("Album Name: " + songData[i].album.name);
+                console.log("Song Preview: " + songData[i].preview_url);
+                console.log("\n----------------------------\n");
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+}
+
