d038a1f50e117c0d3cfa6de52962b658dc40cc22
victor politano <viant3ucf@gmail.com>
2019-08-13 13:10:36 -0400

    added moment - conditions for no user input

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var inquirer = require("inquirer");
 var Spotify = require('node-spotify-api');
+var moment = require("moment");
 
 
 var spotify = new Spotify(keys.spotify);
@@ -27,6 +28,11 @@ switch (action) {
 
 function movieInfo() {
 
+    if(userInput === undefined){
+        userInput = "Mr. Nobody";
+        console.log(" If you havent watched " + userInput + " then you should: http://www.imdb.com/title/tt0485947/ \n It's on Netfilx!");
+    }
+
     axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
             console.log(" Movie Title: " + response.data.Title, "\n",
@@ -47,23 +53,29 @@ function bandInfo() {
 
                 console.log("Artist: " + (response.data[i].lineup));
                 console.log("Venue Name: " + (response.data[i].venue.name));
-                console.log("Venue Location: " + (response.data[i].venue.city) + ", " + (response.data[i].venue.region) + ", " + (response.data[i].venue.country), "\n");
+                console.log("Venue Location: " + (response.data[i].venue.city) + ", " + (response.data[i].venue.region) + ", " + (response.data[i].venue.country));
+                console.log("Venue Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"), "\n");
+                console.log("\n-------------------\n")
             }
         });
 }
 
 function spotifyMe() {
+        if(userInput === undefined){
+            userInput = "The Sign"
+        }
+
     spotify
         .search({ type: 'track', query: userInput, limit: 10 })
         .then(function (response) {
-            // console.log(response);
+         
             var songData = response.tracks.items;
 
             for (let i = 0; i < songData.length; i++) {
-                console.log("Artist Name: " + songData[i].artists[0].name);
-                console.log("Song Name: " + songData[i].name);
-                console.log("Album Name: " + songData[i].album.name);
-                console.log("Song Preview: " + songData[i].preview_url);
+                console.log("Artist Name: " + (songData[i].artists[0].name));
+                console.log("Song Name: " + (songData[i].name));
+                console.log("Album Name: " + (songData[i].album.name));
+                console.log("Song Preview: " + (songData[i].preview_url));
                 console.log("\n----------------------------\n");
             }
         })
