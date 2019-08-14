5104d11d38bc8b7bf3fe318d333ce42cd28d5ca2
victor politano <viant3ucf@gmail.com>
2019-08-14 06:11:27 -0400

    added fs append - need to fix doWhat function

--- liri.js
+++ liri.js
@@ -24,13 +24,16 @@ switch (action) {
         break;
     case "do-what-it-says":
         doWhat();
+        break;
     default:
+        console.log("Something is not right... Try Again")
         break;
 }
 
+
 function movieInfo() {
 
-    if(userInput === undefined){
+    if (userInput === undefined) {
         userInput = "Mr. Nobody";
         console.log(" If you havent watched " + userInput + " then you should: http://www.imdb.com/title/tt0485947/ \n It's on Netfilx!");
     }
@@ -43,7 +46,23 @@ function movieInfo() {
                 "Country of Movie Production: " + response.data.Country, "\n",
                 "Movie Language: " + response.data.Language, "\n",
                 "Movie Plot: " + response.data.Plot, "\n",
-                "Movie Actors: " + response.data.Actors);
+                "Movie Actors: " + response.data.Actors, "\n",
+                "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+
+            fs.appendFile("log.txt", "Movie Title: " + response.data.Title + "\n" +
+                "Year movie was released: " + response.data.Year + "\n" +
+                "IMDB Rating: " + response.data.imdbRating + "\n" +
+                "Country of Movie Production: " + response.data.Country + "\n" +
+                "Movie Language: " + response.data.Language + "\n" +
+                "Movie Plot: " + response.data.Plot + "\n" +
+                "Movie Actors: " + response.data.Actors + "\n" +
+                "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n" +
+                "\n************************************************\n", function (err) {
+
+                    if (err) {
+                        console.log(err);
+                    }
+                });
 
         });
 }
@@ -58,19 +77,30 @@ function bandInfo() {
                 console.log("Venue Location: " + (response.data[i].venue.city) + ", " + (response.data[i].venue.region) + ", " + (response.data[i].venue.country));
                 console.log("Venue Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"), "\n");
                 console.log("\n-------------------\n")
+
+                fs.appendFile("log.txt", "Artist: " + (response.data[i].lineup) + "\n" +
+                    "Venue Name: " + (response.data[i].venue.name) + "\n" +
+                    "Venue Location: " + (response.data[i].venue.city) + ", " + (response.data[i].venue.region) + ", " + (response.data[i].venue.country) + "\n" +
+                    "Venue Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY") + "\n" +
+                    "\n************************************************\n", function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                    });
             }
         });
 }
 
-function spotifyMe(songName) {
-        if(userInput === undefined){
-            userInput = "The Sign"
-        }
+function spotifyMe() {
+    if (userInput === undefined) {
+        userInput = "The Sign"
+    }
 
     spotify
-        .search({ type: 'track', query: userInput, limit: 10 })
+        .search({ type: 'track', query: userInput, limit: 3 })
         .then(function (response) {
-         
+
             var songData = response.tracks.items;
 
             for (let i = 0; i < songData.length; i++) {
@@ -79,6 +109,16 @@ function spotifyMe(songName) {
                 console.log("Album Name: " + (songData[i].album.name));
                 console.log("Song Preview: " + (songData[i].preview_url));
                 console.log("\n----------------------------\n");
+
+                fs.appendFile("log.txt", "Artist Name: " + (songData[i].artists[0].name) + "\n" +
+                    "Song Name: " + (songData[i].name) + "\n" +
+                    "Album Name: " + (songData[i].album.name) + "\n" +
+                    "Song Preview: " + (songData[i].preview_url) + "\n" +
+                    "\n*********************************************\n", function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
             }
         })
         .catch(function (err) {
@@ -87,11 +127,13 @@ function spotifyMe(songName) {
 
 }
 
-function doWhat(){
-    fs.readFile('random.txt', "utf8", function(error, data){
-      var txt = data.split(',');
-  
-      spotifyMe(txt[1]);
+function doWhat() {
+    fs.readFile('random.txt', "utf8", function (error, data) {
+
+        var txtArray = data.split(",");
+    
+
+        spotifyMe(txtArray);
     });
-  }
+}
 
